b4629df2f1f900251a50ace7875289d1b08d9b4a
99aa66 <emanuelecirronis123@gmail.com>
2023-05-14 00:31:06 +0200

    attaque

---
+++ Assets/knockBack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class knockBack : MonoBehaviour
+{
+    [SerializeField] private float pushForce;
+    // Start is called before the first frame update
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+
+        if (rb != null && collision.gameObject.CompareTag("Player"))
+        {
+            Vector3 direction = collision.transform.position - transform.position;
+            direction.y = 0;
+
+            rb.AddForce(direction.normalized * pushForce, ForceMode.Impulse);
+        }   
+    }
+}
