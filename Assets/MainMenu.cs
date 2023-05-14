a5e3c10e22c44941fd09ccf5227e1d91e58fb3a4
jmorquin <j.morquin@mtp.ecolescreatives.com>
2023-05-14 14:04:56 +0200

    Main_Menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,8 +6,15 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
-    
+
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+
+    {
+        Debug.Log("QUIT !");
+        Application.Quit();
     }
 }
